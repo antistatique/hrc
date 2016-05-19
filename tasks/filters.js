056f7042e359d243f1f88f443f4178c9af4edc9e
Yann Gouffon <github@yago-art.ch>
2016-05-19 16:31:51 +0200

    add first homepage contents

--- tasks/filters.js
+++ tasks/filters.js
@@ -14,10 +14,19 @@ module.exports = function() {
     return splited[splited.length-1];
   });
 
-  swig.setFilter('spaceToDash', function (string) {
-    return string.split(' ').join('-');
+  swig.setFilter('phoneRaw', function (number) {
+    var string = number.toString();
+    return '0041' + string;
   });
 
+  swig.setFilter('phoneFancy', function (number) {
+    var string = number.toString();
+    return '0' + string.slice(0, 2) + '&nbsp;' + string.slice(2, 5) + '&nbsp;' + string.slice(5, 7) + '&nbsp;' + string.slice(7);
+  });
+
+  swig.setFilter('titleToUrl', function (string) {
+    return string.split(' ').join('-').toLowerCase();
+  });
 
   swig.setFilter('dump', function (input) {
     return JSON.stringify(input, null, 2);
