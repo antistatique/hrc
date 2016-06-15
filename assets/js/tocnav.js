f045d244a4a2ef403713e7455197f0e2c605546d
Yann Gouffon <github@yago-art.ch>
2016-06-15 11:26:49 +0200

    #35 fit to wireframe for mobile tocnav

--- assets/js/tocnav.js
+++ assets/js/tocnav.js
@@ -5,14 +5,29 @@ const wikiNavInit = ($) => {
     const $container = $('#tocnav-content'),
           $list = $('#tocnav-list');
 
-    $container.find('h1, h2, h3, h4, h5, h6').each(function(i) {
-      const that = $(this),
-            tag = that.prop('tagName').toLowerCase();
+    if ($(window).width() > 767) {
 
-      that.attr('id', `title-${i}`);
+      $container.find('h1, h2, h3, h4, h5, h6').each(function(i) {
+        const that = $(this),
+              tag = that.prop('tagName').toLowerCase();
 
-      $list.append(`<li class="toc-item-${tag}"><a href="#title-${i}">${that.html()}</a></li>`);
-    });
+        that.attr('id', `title-${i}`);
+
+        $list.append(`<li class="toc-item-${tag}"><a href="#title-${i}">${that.html()}</a></li>`);
+      });
+    } else {
+      $container.find('h3').each(function (i) {
+        $(this)
+          .attr({
+            class: 'collapsed',
+            href: `#collapse-${i}`,
+            'data-toggle': 'collapse',
+            'aria-expanded': false
+          })
+          .append('<span class="chevron chevron-up pull-right"></span>');
+        $(this).nextUntil('h3').wrapAll(`<div class='collapse' id='collapse-${i}' />`);
+      });
+    }
   }
 };
 
