f87fa861f986ba1448ae5cdaeb0456d27de27cc7
Yann Gouffon <github@yago-art.ch>
2016-09-09 10:21:25 +0200

    Update 2016-09-09T08:21:22.961Z

--- build/js/main.js
+++ build/js/main.js
@@ -19,6 +19,50 @@ var coverCaption = function coverCaption($) {
 coverCaption(jQuery);
 'use strict';
 
+/* global jQuery, PhotoSwipe, PhotoSwipeUI_Default */
+
+var gallery = function gallery($) {
+  if ($('#gallery').length > 0) {
+    (function () {
+
+      // Init empty gallery array
+      var container = [];
+
+      // Loop over gallery items and push it to the array
+      $('#gallery').find('figure').each(function () {
+        var $link = $(this).find('a'),
+            item = {
+          src: $link.attr('href'),
+          w: $link.data('width'),
+          h: $link.data('height'),
+          title: $link.data('caption')
+        };
+        container.push(item);
+      });
+
+      // Define click event on gallery item
+      $('#gallery a').click(function (event) {
+        // Prevent location change
+        event.preventDefault();
+        // Define object and gallery options
+        var $pswp = $('.pswp')[0],
+            options = {
+          index: $(this).parent('figure').index(),
+          bgOpacity: 0.9,
+          showHideOpacity: true,
+          shareButtons: [{ id: 'download', label: 'Télécharger', url: '{{raw_image_url}}', download: true }]
+        };
+        // Initialize PhotoSwipe
+        var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+        gallery.init();
+      });
+    })();
+  }
+};
+
+gallery(jQuery);
+'use strict';
+
 /* global jQuery */
 
 var coverCaption = function coverCaption($) {
